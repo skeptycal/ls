e897dc926211ab9e9fae4f8bf40e337988f82981
Michael Treanor <skeptycal@gmail.com>
2022-03-26 01:28:02 -0500

    organize repo

---
+++ ansi.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+const ansiFMT string = "\u001b[%dm"
+
+type Color string
+
+const (
+	ColorBlack  Color = "\u001b[30m"
+	ColorRed    Color = "\u001b[31m"
+	ColorGreen  Color = "\u001b[32m"
+	ColorYellow Color = "\u001b[33m"
+	ColorBlue   Color = "\u001b[34m"
+	ColorReset  Color = "\u001b[0m"
+)
+
+func (c Color) String() string {
+	return string(c)
+}
+
+func CPrint(color Color, message string) {
+	fmt.Println(color, message, ColorReset)
+}
